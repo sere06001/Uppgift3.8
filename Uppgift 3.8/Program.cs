b82dc97cb3d3efd8e34172ea583d0936c74efa95
sere06001 <sere06001@utb.vaxjo.se>
2023-10-09 00:47:10 +0200

    Add project files.

---
+++ Uppgift 3.8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift3_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett ord.");
+            string ord1 = Console.ReadLine();
+            Console.WriteLine("Skriv ett andra ord.");
+            string ord2 = Console.ReadLine();
+            Console.WriteLine("Skriv ett tredje ord.");
+            string ord3 = Console.ReadLine();
+
+            List<string> lista = new List<string>();
+            lista.Add(ord1);
+            lista.Add(ord2);
+            lista.Add(ord3);
+            lista.Sort();
+            Console.WriteLine($"{lista[0]} är det ordet som kommer först i bokstavs ordning.");
+            Console.ReadKey();
+        }
+    }
+}
